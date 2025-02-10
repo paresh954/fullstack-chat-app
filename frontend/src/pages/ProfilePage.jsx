08f5f19784b8c5880230a34644c7da57ba1472d3
paresh954 <dakiparesh84@gmail.com>
2025-02-10 16:24:25 +0530

    change the img path in profilepage

--- frontend/src/pages/ProfilePage.jsx
+++ frontend/src/pages/ProfilePage.jsx
@@ -31,11 +31,7 @@ const ProfilePage = () => {
           <div className="flex flex-col items-center gap-4">
             <div className="relative">
               <img
-                src={
-                  selectedImg ||
-                  authUser.profilePic ||
-                  "../../public/avatar.png"
-                }
+                src={selectedImg || authUser.profilePic || "/avatar.png"}
                 alt="Profile"
                 className="size-32 rounded-full object-cover border-4 "
               />
